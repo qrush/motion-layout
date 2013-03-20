43b04fe2eb4bed98f86e6a1eb843ad1e095c6db3
Nick Quaranto <nick@quaran.to>
2013-03-20 17:23:41 -0400

    Bring over timer example from HipByte/RubyMotionSamples

---
+++ example/app/timer_controller.rb
@@ -0,0 +1,61 @@
+#  Copyright (c) 2012 HipByte SPRL and Contributors
+#
+#  Permission is hereby granted, free of charge, to any person
+#  obtaining a copy of this software and associated documentation
+#  files (the “Software”), to deal in the Software without
+#  restriction, including without limitation the rights to use,
+#  copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following
+#  conditions:
+#
+#  The above copyright notice and this permission notice shall be
+#  included in all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#  OTHER DEALINGS IN THE SOFTWARE. 
+
+class TimerController < UIViewController
+  attr_reader :timer
+
+  def viewDidLoad
+    margin = 20
+
+    @state = UILabel.new
+    @state.font = UIFont.systemFontOfSize(30)
+    @state.text = 'Tap to start'
+    @state.textAlignment = UITextAlignmentCenter
+    @state.textColor = UIColor.whiteColor
+    @state.backgroundColor = UIColor.clearColor
+    @state.frame = [[margin, 200], [view.frame.size.width - margin * 2, 40]]
+    view.addSubview(@state)
+
+    @action = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @action.setTitle('Start', forState:UIControlStateNormal)
+    @action.setTitle('Stop', forState:UIControlStateSelected)
+    @action.addTarget(self, action:'actionTapped', forControlEvents:UIControlEventTouchUpInside)
+    @action.frame = [[margin, 260], [view.frame.size.width - margin * 2, 40]]
+    view.addSubview(@action)
+  end
+
+  def actionTapped
+    if @timer
+      @timer.invalidate
+      @timer = nil
+    else
+      @duration = 0
+      @timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target:self, selector:'timerFired', userInfo:nil, repeats:true)
+    end
+    @action.selected = !@action.selected?
+  end
+
+  def timerFired
+    @state.text = "%.1f" % (@duration += 0.1)
+  end
+end

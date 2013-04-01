6deeae45bf2b01582a87dd778c191a84e69bfc90
Darrin Holst <darrinholst@gmail.com>
2013-04-01 09:09:11 -0500

    add layout options to readme and to the example app

--- example/app/timer_controller.rb
+++ example/app/timer_controller.rb
@@ -25,12 +25,9 @@ class TimerController < UIViewController
   attr_reader :timer
 
   def viewDidLoad
-    @state = UILabel.new
-    @state.font = UIFont.systemFontOfSize(30)
-    @state.text = 'Tap to start'
-    @state.textAlignment = UITextAlignmentCenter
-    @state.textColor = UIColor.whiteColor
-    @state.backgroundColor = UIColor.clearColor
+    @state = buildLabelWithText('Tap to start', ofSize: 30)
+    @version = buildLabelWithText('version', ofSize: 14)
+    @versionNumber = buildLabelWithText('0.0.1', ofSize: 19)
 
     @action = UIButton.buttonWithType(UIButtonTypeRoundedRect)
     @action.setTitle('Start', forState:UIControlStateNormal)
@@ -39,11 +36,13 @@ class TimerController < UIViewController
 
     Motion::Layout.new do |layout|
       layout.view view
-      layout.subviews "state" => @state, "action" => @action
+      layout.subviews "state" => @state, "action" => @action, "version" => @version, "version_number" => @versionNumber
       layout.metrics "top" => 200, "margin" => 20, "height" => 40
       layout.vertical "|-top-[state(==height)]-margin-[action(==height)]"
+      layout.vertical "[version]-|"
       layout.horizontal "|-margin-[state]-margin-|"
       layout.horizontal "|-margin-[action]-margin-|"
+      layout.horizontal "[version]-5-[version_number]-|", NSLayoutFormatAlignAllBaseline
     end
   end
 
@@ -60,5 +59,15 @@ class TimerController < UIViewController
 
   def timerFired
     @state.text = "%.1f" % (@duration += 0.1)
+  end
+
+  def buildLabelWithText(text, ofSize: size)
+    label = UILabel.new
+    label.font = UIFont.systemFontOfSize(size)
+    label.text = text
+    label.textAlignment = UITextAlignmentCenter
+    label.textColor = UIColor.whiteColor
+    label.backgroundColor = UIColor.clearColor
+    label
   end
 end
